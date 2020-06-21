fe9dd2fe973dc8d4355fc109a31218af5454c324
morganpstanley <morganpstanley@gmail.com>
2020-06-20 20:10:29 -0600

    create and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,18 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([ name: 'Star Wars' ,  name: 'Lord of the Rings' ])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+morgan = User.create(username: 'Morgan', password: 'password')
+john = User.create(username: 'John', password: '123')
+
+Stock.create(name: 'NKE', user_id: 1, purchase_price: 90, purchase_amount: 10)
+Stock.create(name: 'AAPL', user_id: 1, purchase_price: 350, purchase_amount: 5)
+Stock.create(name: 'AMZN', user_id: 1, purchase_price: 2500, purchase_amount: 1)
+
+Stock.create(name: 'NKE', user_id: 2, purchase_price: 90, purchase_amount: 15)
+Stock.create(name: 'AAPL', user_id: 2, purchase_price: 350, purchase_amount: 9)
+Stock.create(name: 'AMZN', user_id: 2, purchase_price: 2500, purchase_amount: 4)
+Stock.create(name: 'UAL', user_id: 2, purchase_price: 33, purchase_amount: 80)
