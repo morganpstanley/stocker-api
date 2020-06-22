7fb006058f49a781a731316d95009543ede3a66a
morganpstanley <morganpstanley@gmail.com>
2020-06-22 15:33:50 -0600

    add stock and users basic controllers

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,36 @@
 class StocksController < ApplicationController
+
+    def index
+        stocks = Stock.all
+        render json: stocks
+    end
+
+    def create
+        stock = Stock.create(
+            name: params[:name], 
+            user_id: params[:user_id],
+            purchase_amount: params[:purchase_amount],
+            purchase_price: params[:purchase_price]
+        )
+        render json: stock
+    end
+
+    def update
+        stock = Stock.find_by_id(params[:id])
+        stock.update(stock_params)
+        render json: stock
+    end
+
+    def destroy
+        stock = Stock.find_by_id(params[:id])
+        Stock.destroy(params[:id])
+    end
+    
+    
+private
+
+    def stock_params
+        params.permit(:name, :user_id, :purchase_amount, :purchase_price)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def create
+        @ser = User.new(user_params) 
+        if params[:user][:password] == params[:user][:password_confirmation] && @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end  
+    end
+
+private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+    
 end
