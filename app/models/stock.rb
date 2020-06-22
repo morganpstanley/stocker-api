9eb7b2eb5fb8912d0110e990556a464e6e78502c
morganpstanley <morganpstanley@gmail.com>
2020-06-22 15:33:16 -0600

    fix spelling error

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
     belongs_to :user
 
-    validates :name, :purchase_amount, :purchase_price, :user_id, presense: true
+    validates :name, :purchase_amount, :purchase_price, :user_id, presence: true
 end
