996707818b4ac70803de76397d33c3e5872fd5ae
morganpstanley <morganpstanley@gmail.com>
2020-06-23 16:23:44 -0600

    add user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,24 @@
 class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
 
+    def index
+        @users = User.all
+        render json: @users
+    end
+
     def create
-        user = User.new(user_params) 
-        if params[:user][:password] == params[:user][:password_confirmation] && user.save
-            session[:user_id] = user.id
+        @user = User.new(user_params) 
+        if params[:user][:password] == params[:user][:password_confirmation] && @user.save
+            login!
             render json: {
                 status: 'created',
-                user: user
+                user: @user
             }
         else
-            render json: {status: 505 }
+            render json: {
+                status: 505,
+                errors: @user.errors.full_messages 
+            }
         end  
     end
 
