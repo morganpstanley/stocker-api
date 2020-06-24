3c9abdb7873351eea46543b966f309bb5b0c25eb
morganpstanley <morganpstanley@gmail.com>
2020-06-23 18:57:27 -0600

    add login, logout, and logged_in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :stocks
   resources :users
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  
+  resources :users, only: [:create, :show, :index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
