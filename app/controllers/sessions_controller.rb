1de7ab1c157f0ae98108ad969870fadd3203fbaa
morganpstanley <morganpstanley@gmail.com>
2020-07-03 18:09:12 -0600

    refactor - remove unnecessary login path in session controller"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,5 @@
 class SessionsController < ApplicationController
-
-    def login
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-             redirect_to user_path(@user)
-        else
-            redirect_to :login
-        end
-    end
+    skip_before_action :verify_authenticity_token
 
     def create
        @user = User.find_by(username: session_params[:username])
