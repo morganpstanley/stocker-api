07897342b40b75d1da86f841b13d9457685c845c
morganpstanley <morganpstanley@gmail.com>
2020-07-03 18:07:48 -0600

    remove skip_before_action in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-     skip_before_action :verify_authenticity_token
 
     helper_method :login!, :logged_in?, :current_user,
         :authorized_user?, :logout!
